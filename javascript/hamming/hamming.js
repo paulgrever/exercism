7d73cd4ec89613bb453ea82be867cf9960e8bbdc
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2016-02-26 15:00:34 -0700

    all hamming pass

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,7 +1,16 @@
 function Hamming () {};
 
 Hamming.prototype.compute = function (DNA1, DNA2) {
-  return 0;
+  if (DNA1.length !== DNA2.length) {
+    throw new Error('DNA strands must be of equal length.');
+  }
+  var count = 0;
+  for(var i = 0; i < DNA1.length; i++) {
+    if (DNA1.charAt(i) !== DNA2.charAt(i)) {
+      count += 1;
+    }
+  }
+  return count;
 };
 
 module.exports = Hamming;
