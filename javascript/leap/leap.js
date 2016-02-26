cb4985d3029379d639e722f17675366027663c36
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2016-02-26 14:16:10 -0700

    4 tests passing

--- javascript/leap/leap.js
+++ javascript/leap/leap.js
@@ -1,9 +1,9 @@
-var Year = function (year) {
+function Year (year) {
   this.year = year;
 };
 
 Year.prototype.isLeap = function() {
-  return (this.year % 4 === 0);
+  return (this.year % 400 === 0) || ((this.year % 4 === 0) && (this.year % 100 !== 0));
 };
 
 module.exports = Year;
