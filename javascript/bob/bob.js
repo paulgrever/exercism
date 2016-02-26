d618c8d41ac98a5e0a9aaff90f9a24617c72e625
Paul Grever <paulgrever@Pauls-MacBook-Pro-2.local>
2016-02-26 16:46:17 -0700

    all 17 tests pass bob

---
+++ javascript/bob/bob.js
@@ -0,0 +1,19 @@
+//
+// This is only a SKELETON file for the "Bob" exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+var Bob = function() {};
+
+Bob.prototype.hey = function(input) {
+  if (input === input.toUpperCase() && /[A-Z]/.test(input)) {
+    return 'Whoa, chill out!';
+  } else if (input[input.length - 1] === '?') {
+    return 'Sure.'
+  } else if (!/\S/.test(input)) {
+    return 'Fine. Be that way!'
+  }
+  return 'Whatever.';
+};
+
+module.exports = Bob;
